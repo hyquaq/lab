7385965e6b2b7f8cf6650dc2725af691c7331053
hyquaq <hyquaq@gmail.com>
2020-11-08 17:30:04 +0700

    add on folder

---
+++ lab6/lab6_exercises6.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+
+int factorialN(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * factorialN(n - 1);
+}
+
+int ansA(int n)
+{
+    if (n == 1)
+        return 3;
+    return (2 * n + 1) + ansA(n - 1);
+}
+
+float ansB(int n)
+{
+    if (n == 1)
+        return 1.0 / 2;
+    return (1.0 / n) + ansB(n - 1);
+}
+
+int ansC(int n)
+{
+    if (n == 1)
+        return 1;
+    return factorialN(n) + ansC(n - 1);
+}
+
+float ansD(int n)
+{
+    if (n == 1)
+        return 1;
+    return sqrt(n * 1.0) + ansD(n - 1);
+}
+
+int ansE(int n)
+{
+    if (n == 1)
+        return 1;
+    return factorialN(n) * ansE(n - 1);
+}
+
+int main(void)
+{
+    int n;
+    printf("enter n = ");
+    scanf("%d", &n);
+    printf("a. 2i+1 == %d\n", ansA(n));
+    printf("a. i/2 == %f\n", ansB(n));
+    printf("a. i! == %d\n", ansC(n));
+    printf("a. sqrt(i) == %f\n", ansD(n));
+    printf("a. *i! == %d\n", ansE(n));
+    return 0;
+}
