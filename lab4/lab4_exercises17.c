7c25ce09bf70f1bece9278f42f11b741c7124a1f
hyquaq <hyquaq@gmail.com>
2020-11-23 14:00:45 +0700

    17

--- lab4/lab4_exercises17.c
+++ lab4/lab4_exercises17.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
-int main()
+#define SIZE 10
+
+void printfArrayMatrix(int a[][SIZE], int columns, int rows)
 {
-    int i, n, sum;
-    printf("enter n : ");
-    scanf("%d", &n);
-    sum = 0;
-    for (i = 1; i <= n; i++)
+    for (int i = 0; i < rows; i++)
     {
-        if (n % 2)
+        for (int j = 0; j < columns; j++)
         {
-            sum += i;
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+void subtractTwoMatrix(int a[][SIZE], int b[][SIZE], int c[][SIZE], int columns, int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            c[i][j] = a[i][j] - b[i][j];
         }
     }
-    printf("\t\t%d", sum);
+}
+int main(void)
+{
+    int columns = 3;
+    int rows = 2;
+    int a[][SIZE] = {
+        {1, 2, 3},
+        {2, 3, 4}};
+    int b[][SIZE] = {
+        {1, 2, 3},
+        {2, 3, 4}};
+    int c[][SIZE] = {0};
+    printf("matrix one: \n");
+    printfArrayMatrix(a, columns, rows);
+    printf("matrix two: \n");
+    printfArrayMatrix(b, columns, rows);
+
+    subtractTwoMatrix(a, b, c, columns, rows);
+    printf("matrix Before subtract: \n");
+    printfArrayMatrix(c, columns, rows);
+
+    return 0;
 }
