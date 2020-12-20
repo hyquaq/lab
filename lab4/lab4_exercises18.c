6bf2afb25ee7c48f711b785fb4d35b3b140f3fb7
hyquaq <hyquaq@gmail.com>
2020-12-20 16:39:05 +0700

    delete

--- lab4/lab4_exercises18.c
+++ lab4/lab4_exercises18.c
@@ -1,7 +1,57 @@
 // Write program to multiply two matrices.
 #include <stdio.h>
+#define SIZE 10
 
+void computeMultiplyTwoMatrices(int a[SIZE][SIZE], int b[SIZE][SIZE], int c[SIZE][SIZE], int x, int y, int z)
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < z; j++)
+        {
+            for (int k = 0; k < y; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
 int main(void)
 {
+    FILE *fi = fopen("input.txt", "r");
+    int a[SIZE][SIZE] = {0};
+    int b[SIZE][SIZE] = {0};
+    int c[SIZE][SIZE] = {0};
+    int x = 2, y = 3, z = 4;
+
+    fscanf(fi, "%d", &x);
+    fscanf(fi, "%d", &y);
+    fscanf(fi, "%d", &z);
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            fscanf(fi, "%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < z; j++)
+        {
+            fscanf(fi, "%d", &b[i][j]);
+        }
+    }
+
+    computeMultiplyTwoMatrices(a, b, c, x, y, z);
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < z; j++)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    fclose(fi);
     return 0;
 }
--- lab6/run.py
+++
@@ -1,4 +0,0 @@
-import os
-
-for i in range(1, 7):
-    os.system(f'gcc lab6_exercises{i}.c -o lab6_exercises{i}.exe')
